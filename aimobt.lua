00e5bedda12cfdd0dedc38082f3a8ac16c054cee
ArtHurLeyQ <107763428+ArtHurLeyQ@users.noreply.github.com>
2022-06-19 15:43:46 +0300

    Create aimobt.lua

---
+++ aimobt.lua
@@ -0,0 +1,39 @@
+local UIS = game:GetService("UserInputService")
+local camera = game.Workspace.CurrentCamera
+local Plr = game.Players.LocalPlayer
+
+
+-- Making Closest Character
+function getClosest()
+    local closestPlayer = nil --
+    local closesDist = math.huge --
+    
+    for i,v in pairs(game.Players:GetPlayers()) do 
+        if v ~= game.Players.LocalPlayer and v.TeamColor ~= Plr.TeamColor then
+            local Dist = (game.Players.LocalPlayer.Character.HumanoidRootPart.Position - v.Character.HumanoidRootPart.Position).magnitude
+            if Dist < closesDist then 
+                closesDist = Dist 
+                closestPlayer = v
+            end 
+        end
+    end
+    return closestPlayer
+end
+
+_G.aim = false
+UIS.InputBegan:Connect(function(inp)
+    if inp.KeyCode == Enum.KeyCode.Z then
+        _G.aim = true
+        while wait() do 
+            camera.CFrame = CFrame.new(camera.CFrame.Position, getClosest().Character.Head.Position)
+            if _G.aim == false then return end
+        end
+    end
+end)
+
+UIS.InputEnded:Connect(function(inp)
+    if inp.KeyCode == Enum.KeyCode.Z then
+        _G.aim = false
+    end
+
+end)
